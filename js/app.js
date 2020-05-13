4eb1f1c8999b98ceba933c781ed1a99ebabc4afd
Eduard-Mychka <edmychka@gmail.com>
2020-05-13 16:24:32 +0300

    new sdn

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 function toggle() {
+  var handleScroll = document.querySelector("body").style;
   var sec = document.getElementById("sec");
   var nav = document.getElementById("navigation");
   sec.classList.toggle("active");
   nav.classList.toggle("active");
+
+  // if (nav.classList.value.includes("active")) {
+  //   handleScroll.overflowY = "hidden";
+  // } else {
+  //   handleScroll.overflowY = "scroll";
+  // }
 }
